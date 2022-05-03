99e7cfb0f82ba256d25ea25b47705421607bc764
rtoktogulov <r.toktogulov@gmail.com>
2022-05-03 19:26:02 +0600

    pagination in categories

--- src/pages/Phones.jsx
+++ src/pages/Phones.jsx
@@ -1,5 +1,6 @@
-import { Container } from "@mui/material";
-import React, { useContext, useEffect } from "react";
+import { Container, Pagination } from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
+
 import MyPagination from "../components/MyPagination";
 import ProductCard from "../components/ProductCard";
 import { clientContext } from "../context/ClientContext";
@@ -7,6 +8,18 @@ import { clientContext } from "../context/ClientContext";
 const Phones = () => {
   const data = useContext(clientContext);
   const { phones, getPhones } = data;
+  const productsPerPage = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = currentPage * productsPerPage - productsPerPage;
+  const phone = phones.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const totalCount = phones.length;
+  const totalPages = Math.ceil(totalCount / productsPerPage);
+
+  const handlePagination = (page) => {
+    setCurrentPage(page);
+  };
 
   useEffect(() => {
     getPhones();
@@ -15,11 +28,18 @@ const Phones = () => {
   return (
     <Container>
       <div className="products-list">
-        {phones.map((item) => (
+        {phone.map((item) => (
           <ProductCard key={item.id} item={item} />
         ))}
       </div>
-      <MyPagination />
+      <div className="my-pagination">
+        <Pagination
+          onChange={(_, page) => handlePagination(page)}
+          count={totalPages}
+          color="primary"
+          // size="small"
+        />
+      </div>
     </Container>
   );
 };
